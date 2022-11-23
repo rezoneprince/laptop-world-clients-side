0d5d2ec384fc6905a0436d85e32ee13fdef20f92
Md Rezone Ahamed <rezoneprinceme@gmail.com>
2022-11-24 05:01:27 +0600

    install some importent packege and setup file system

---
+++ src/routes/Private/AdminRoutes.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AdminRoutes = () => {
+  return <div>Admin Routes</div>;
+};
+
+export default AdminRoutes;
---
+++ src/routes/Private/UserRoutes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserRoutes = () => {
+    return (
+        <div>
+            User Routes
+        </div>
+    );
+};
+
+export default UserRoutes;
